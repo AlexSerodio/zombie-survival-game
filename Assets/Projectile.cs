acd531ca06205349f151a679eda95951cd4bc109
Unknown <alequis01@gmail.com>
2018-03-31 12:40:15 -0300

    Add projectiles shot

---
+++ Assets/Projectile.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	[SerializeField]
+	private float speed;
+
+	void FixedUpdate () {
+		GetComponent<Rigidbody>().MovePosition(
+			GetComponent<Rigidbody>().position + (transform.forward * speed * Time.deltaTime));
+	}
+}
---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class WeaponController : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject projectile;
+	[SerializeField]
+	private Transform shootPosition;
+
+	void Update () {
+		if (Input.GetButtonDown("Fire1")) {
+			Instantiate(projectile, shootPosition.position, shootPosition.rotation);
+		}
+	}
+}
