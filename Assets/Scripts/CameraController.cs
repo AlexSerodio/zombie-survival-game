d737f6b196b1b627675fd15cac34c5cca881332e
Unknown <alequis01@gmail.com>
2018-03-30 22:30:01 -0300

    Add player animation and camera movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	[SerializeField]
+	private Transform player;
+	private Vector3 distance;
+
+	void Start () {
+		distance = transform.position - player.position;
+	}
+
+	void Update () {
+		transform.position = player.position + distance;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField]
+	private float speed;
+
+	void Update () {
+
+		float xAxis = Input.GetAxis("Horizontal");
+		float zAxis = Input.GetAxis("Vertical");
+
+		Vector3 direction = new Vector3(xAxis, 0, zAxis);
+
+		transform.Translate(direction * Time.deltaTime * speed);	
+
+		if (direction != Vector3.zero)
+			GetComponent<Animator>().SetBool("Running", true);
+		else
+			GetComponent<Animator>().SetBool("Running", false);
+	}
+}
