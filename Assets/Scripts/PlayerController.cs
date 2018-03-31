d8bda8b058b49d18a9c3dc7c5df98ba6435383d7
Unknown <alequis01@gmail.com>
2018-03-31 00:05:47 -0300

    Add collision treatment

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,19 +4,29 @@ public class PlayerController : MonoBehaviour {
 
 	[SerializeField]
 	private float speed;
+	private Vector3 direction;
 
 	void Update () {
 
+		// player movement inputs. Stores the X and Z direction using the pressed keys
 		float xAxis = Input.GetAxis("Horizontal");
 		float zAxis = Input.GetAxis("Vertical");
 
-		Vector3 direction = new Vector3(xAxis, 0, zAxis);
+		// creates a Vector3 with the new direction
+		direction = new Vector3 (xAxis, 0, zAxis);
 
-		transform.Translate(direction * Time.deltaTime * speed);	
-
+		// player animations transition
 		if (direction != Vector3.zero)
 			GetComponent<Animator>().SetBool("Running", true);
 		else
 			GetComponent<Animator>().SetBool("Running", false);
+	}
+		
+	void FixedUpdate () {
+		// player movement by second using physics
+		// use physics (rigidbody) to compute the player movement is better than transform.position 
+		// because prevents the player to "bug" when colliding with other objects
+		GetComponent<Rigidbody>().MovePosition(
+			GetComponent<Rigidbody>().position + (direction * Time.deltaTime * speed));
 	}
 }
